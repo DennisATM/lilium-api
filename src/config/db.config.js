3b0eb284581f18d6163fc777ed2689d93c3f9ba5
Dennis Torres <dennisalbertotm@gmail.com>
2026-01-07 16:56:45 -0300

    feat: configurando correctamente el dbConfig.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,43 +1,39 @@
 import { Sequelize } from "sequelize";
 import { config } from "./env.config.js";
-import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const {Pool} = pkg;
+let dbConfig;
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+if (process.env.DATABASE_URL) {
+  // 🔹 Render (usa la URL interna)
+  console.log("Conectando a Render DB...");
+  dbConfig = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // necesario en Render
+      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
+} else {
+  // 🔹 Local (usa tu archivo .env normal)
+  console.log("Conectando a BD local...");
+  const { host, user, pass, name, dialect, port } = config.db;
+  dbConfig = new Sequelize(name, user, pass, {
+    host,
+    port,
+    dialect,
+  });
+}
 
-
-// let dbConfig;
-
-// if (process.env.DATABASE_URL) {
-//   // 🔹 Render (usa la URL interna)
-//   console.log("Conectando a Render DB...");
-//   dbConfig = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false, // necesario en Render
-//       },
-//     },
-//   });
-// } else {
-//   // 🔹 Local (usa tu archivo .env normal)
-//   console.log("Conectando a BD local...");
-//   const { host, user, pass, name, dialect, port } = config.db;
-//   dbConfig = new Sequelize(name, user, pass, {
-//     host,
-//     port,
-//     dialect,
-//   });
-// }
-
-// export { dbConfig };
+export { dbConfig };
